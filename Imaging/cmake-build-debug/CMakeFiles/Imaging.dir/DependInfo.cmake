098a2d00d8e7670f4bc767d6dfd2e7df45e8dff0
mariekevesch98 <marieke.vanesch@ziggo.nl>
2022-04-22 16:19:47 +0200

    Initial commit

---
+++ Imaging/cmake-build-debug/CMakeFiles/Imaging.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/imaging.cpp" "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/cmake-build-debug/CMakeFiles/Imaging.dir/imaging.cpp.o"
+  "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/main.cpp" "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/cmake-build-debug/CMakeFiles/Imaging.dir/main.cpp.o"
+  "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/viewer.cpp" "/mnt/c/Users/marie/Documents/q4geomatics/geo1016/Lab_1_Imaging_b_WithWiewer/Lab_1_Imaging_b_WithWiewer/Imaging/cmake-build-debug/CMakeFiles/Imaging.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Imaging/cmake-build-debug/CMakeFiles/Imaging.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Imaging.dir/imaging.cpp.o"
+  "CMakeFiles/Imaging.dir/main.cpp.o"
+  "CMakeFiles/Imaging.dir/viewer.cpp.o"
+  "Imaging"
+  "Imaging.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Imaging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
